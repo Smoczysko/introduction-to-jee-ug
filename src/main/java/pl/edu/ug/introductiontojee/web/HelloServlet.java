7132ad211ee15cf46038d0ab44dfc3404e9cb66a
lukasz.rybka@solwit.pl <lukasz.rybka@solwit.pl>
2014-10-12 20:54:34 +0200

    Initial project structure

---
+++ src/main/java/pl/edu/ug/introductiontojee/web/HelloServlet.java
@@ -0,0 +1,19 @@
+package pl.edu.ug.introductiontojee.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html><body><h2>Hello!</h2></body></html>");
+        writer.close();
+    }
+}
