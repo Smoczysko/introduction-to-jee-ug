9962f53e6ece5defc19396e2c12ad9cf9f4d68ad
lukasz.rybka@solwit.pl <lukasz.rybka@solwit.pl>
2014-10-27 21:45:48 +0100

    Removed web.xml file.

--- src/main/java/pl/edu/ug/introductiontojee/web/HelloServlet.java
+++ src/main/java/pl/edu/ug/introductiontojee/web/HelloServlet.java
@@ -1,12 +1,14 @@
 package pl.edu.ug.introductiontojee.web;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet(urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
